2447d950460c1cbdbf11671681ae8ba18c604e99
ShayanJamil519 <shayanjamil500@gmail.com>
2023-06-19 19:13:14 +0500

    Package added

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-        .then((data) => {
-            console.log(`Monogodb connected with server : ${data.connection.host} `)
-        })
-
-}
+  // mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  mongoose
+    .connect(
+      " mongodb+srv://new-user_31:aLOc9DLOZDAXAoKq@ecommerce.ilhzz.mongodb.net/Ecommerce?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+    )
+    .then((data) => {
+      console.log(`Monogodb connected with server : ${data.connection.host} `);
+    });
+};
 
 // module.exports = connectDatabase
-module.exports = connectDatabase
+module.exports = connectDatabase;
