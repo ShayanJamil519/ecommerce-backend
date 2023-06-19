f643cb60e6af9d60f515c910387ea335664bb959
ShayanJamil519 <shayanjamil500@gmail.com>
2023-06-19 20:00:25 +0500

    ...

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
